5971d24ffea16f4093cc521667fa60968aef64f8
Ec da KreatOr <64685270+Kreat0r-0503@users.noreply.github.com>
2020-11-12 09:47:42 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
+        <!-- Created with SVG-edit - https://github.com/SVG-Edit/svgedit-->
+        <g class="layer">
+         <title>Layer 1</title>
+         <circle  cx="38.5" cy="40" fill="#000000" id="svg_3" r="38.91488" stroke="#000000" stroke-width="0">
+         <animate attributeType="CSS" attributeName="stroke-width"
+             from="500" to="1" dur="1s" repeatCount="1"/>
+        </circle>
+         <circle class="c2" cx="97" cy="93.5" fill="#000000" id="svg_9" r="38.91488" stroke="#000000" stroke-width="0" transform="matrix(1 0 0 1 0 0)">
+            <animate attributeType="CSS" attributeName="stroke-width"
+            from="500" to="1" dur="1s" repeatCount="1"/>
+            </circle>
+         <circle class="c3" cx="152" cy="151.5" fill="#000000" id="svg_10" r="38.91488" stroke="#000000" stroke-width="0" transform="matrix(1 0 0 1 0 0)">
+            <animate attributeType="CSS" attributeName="stroke-width"
+            from="500" to="1" dur="1s" repeatCount="1"/>
+            </circle>
+         <circle class="c4" cx="209" cy="206.5" fill="#000000" id="svg_11" r="38.91488" stroke="#000000" stroke-width="0" transform="matrix(1 0 0 1 0 0)">
+            <animate attributeType="CSS" attributeName="stroke-width"
+            from="500" to="1" dur="1s" repeatCount="1"/>
+            </circle>
+         <circle class="c5" cx="264" cy="262.5" fill="#000000" id="svg_12" r="38.91488" stroke="#000000" stroke-width="0">
+            <animate attributeType="CSS" attributeName="stroke-width"
+            from="500" to="1" dur="1s" repeatCount="1"/>
+            </circle>
+         <circle class="c6" cx="257" cy="38.5" fill="#000000" id="svg_13" r="38.91488" stroke="#000000" stroke-width="0">
+            <animate attributeType="CSS" attributeName="stroke-width"
+            from="500" to="1" dur="1s" repeatCount="1"/>
+            </circle>
+         <circle class="c7" cx="204" cy="92.5" fill="#000000" id="svg_14" r="38.91488" stroke="#000000" stroke-width="0" transform="matrix(1 0 0 1 0 0)">
+            <animate attributeType="CSS" attributeName="stroke-width"
+            from="500" to="1" dur="1s" repeatCount="1"/>
+            </circle>
+         <circle class="c8" cx="93" cy="205.5" fill="#000000" id="svg_15" r="38.91488" stroke="#000000" stroke-width="0" transform="matrix(1 0 0 1 0 0)">
+            <animate attributeType="CSS" attributeName="stroke-width"
+            from="500" to="1" dur="1s" repeatCount="1"/>
+            </circle>
+         <circle class="c9" cx="36" cy="259.5" fill="#000000" id="svg_16" r="38.91488" stroke="#000000" stroke-width="0">
+            <animate attributeType="CSS" attributeName="stroke-width"
+            from="500" to="1" dur="1s" repeatCount="1"/>
+            </circle>
+        </g>
+       </svg>
+    </body>
+    </html>
+    
+    <style>
+    html {
+    background: beige;
+    height: 100%;
+    overflow: hidden;
+    text-align: center;
+  }
+    </style>
